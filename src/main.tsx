94e3c85c8421c8a50e8f6209ea1b4fd602df0dcb
Wargas <teixeira.wargas@gmail.com>
2022-09-22 16:25:33 -0300

    sw

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from "./App";
 import "./index.css";
@@ -9,3 +10,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register({onUpdate: () => {}, onSuccess: () => {}});
